4d5578faa1358a3e320a4c4e511abb828e2ac637
Araina350 <aishwarya.agarwal20@gmail.com>
2025-02-05 15:43:54 +0530

    initial commit

--- first.py
+++ first.py
@@ -1,3 +1,11 @@
 #print("Good afternoon")
 print('This is Araina\'s code')
 print("the sum of 13457 and 45632 is",13457+45632)
+num1=23456789012356
+num2=98765432109768
+num3=num4=123456777899575
+print(num1+num2+num3)
+name="Araina"
+print("Hello" , name)
+year=input("Enter your birth year ")
+print(name," was born in ", year)
