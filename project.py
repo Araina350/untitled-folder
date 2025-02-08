eb24fe23cb2401bc2b3ba74281148147f95e43da
Araina350 <aishwarya.agarwal20@gmail.com>
2025-02-08 12:41:57 +0530

    initial commit

---
+++ project.py
@@ -0,0 +1,10 @@
+ArainaBirth="15/12/2014"
+AarickBirth="9/9/2020"
+AvantikaBirth="28/12/2013"
+TarishiBirth="23/1/2017"
+ReedhimaBirth="15/1/2015"
+print("Araina was born on ",ArainaBirth,"." )
+print("Aarick was born on ",AarickBirth,"." )
+print("Avantika was born on ",AvantikaBirth,"." )
+print("Tarishi was born on ",TarishiBirth,"." )
+print("Reedhima was born on ",ReedhimaBirth,"." )
