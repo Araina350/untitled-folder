4eb40e96bea6f1a1a3fad37042a69a076cdcdec7
Araina350 <aishwarya.agarwal20@gmail.com>
2025-02-10 15:54:00 +0530

    initial commit

---
+++ datatype.py
@@ -0,0 +1,26 @@
+rollNo=5
+print(type(rollNo))
+
+Name="Araina"
+print(type(Name))
+
+rupees_left=1234.1
+print(type(rupees_left))
+
+result=True
+print(type(result))
+
+# num1=int(input("Enter the first number."))
+# num2=int(input("Enter the second number."))
+# sum=num1+num2
+# print(sum)
+
+s1="Hello"
+s2="World"
+print(s1+" "+s2)
+pyt="Welcome to the world of Python"
+print(len(pyt))
+
+str="Computers"
+print(str[0:4])
+print(str[::-1])
