ca01784227492c3de5df5c1c4eca0b45643c3775
Araina350 <aishwarya.agarwal20@gmail.com>
2025-05-16 14:30:02 +0530

    initial commit

---
+++ inter.py
@@ -0,0 +1,8 @@
+inter={"green","blue"}
+section={"blue","yellow"}
+print("Original set elements")
+print(inter)
+print(section)
+print("\n INTERSECTION OF THE TWO SAID SETS:")
+pro=inter.intersection(section)
+print(pro)
