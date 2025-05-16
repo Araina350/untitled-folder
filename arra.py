8c30c1d2fad9f8f1981ac82ae4971660a02a3abf
Araina350 <aishwarya.agarwal20@gmail.com>
2025-05-16 14:48:12 +0530

    initial commit

---
+++ arra.py
@@ -0,0 +1,6 @@
+import array as arr
+array_num=arr.array("i",[1,3,5,3,7,9,3])
+print("NUMBER OF OCCURENCES OF 3 IN THE GIVEN ARRAY:"+str(array_num.count(3)))
+array_num.reverse()
+print("REVERSED ORDER OF THE ITEMS :")
+print(str(array_num))
