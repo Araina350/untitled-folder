7f888885a02776df6872657608233dd54f634cd9
Araina350 <aishwarya.agarwal20@gmail.com>
2025-05-16 14:22:51 +0530

    initial commit

---
+++ s.py
@@ -0,0 +1,13 @@
+my_set={1,2,3,4}
+print(my_set)
+mix_set={1.1,"HOLA",1,True}
+print(mix_set)
+double_set={1,2,3,4,3,2}
+print(double_set)
+set4=set([1,2,3,2])
+rem_set={0,1,3,4,5}
+print("THE ORIGINAL SET:" )
+print(rem_set)
+rem_set.pop()
+print("NEW SET:")
+print(rem_set,"\n")
