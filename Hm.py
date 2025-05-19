28e0d2a37db479cb948ee2ce0c5cab6814374e60
Araina350 <aishwarya.agarwal20@gmail.com>
2025-05-19 14:32:27 +0530

    initial commit

---
+++ Hm.py
@@ -0,0 +1,11 @@
+n1=[1,2,3]
+n2=[4,5,6]
+result=map(lambda x,y:x+y,n1,n2)
+print("Addition of two lists")
+print(list(result))
+nums=[1,2,3,4,5,]
+def sq(n):
+    return n*n
+square=list(map(sq,nums))
+print("SQUARE OF NUMBERS IN LIST")
+print(square)
